0915bb3a86a0608a9bd24f60e864fae9e22d42d7
okaka <aki.na@live.jp>
2022-03-17 11:05:27 +0900

    T0316.java

---
+++ day2/T0316.java
@@ -0,0 +1,14 @@
+public class T0316{
+	public static void main(String[] args){
+		String name="ゴキブリ";
+		int num=10000;
+		System.out.println(name+"が"+num+"匹生きている");
+		System.out.println(name+"が半分死んだ");
+		num = num / 2;
+		System.out.println(name+"が"+num+"匹生きている");
+		System.out.println(name+"がさらに半分死んだ");
+		num = num / 2;
+		System.out.println(name+"が"+num+"匹生きている");
+
+	}
+}
